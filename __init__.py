00cd5c615e6d24ba3043f1bcea1edbde4cf42d24
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2020-11-24 09:56:01 -0300

    03.01_Criando_Model_Conta

--- __init__.py
+++ __init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
+mi = Migrate(app, db)
+
+from .models import conta_model
---
+++ models/conta_model.py
@@ -0,0 +1,9 @@
+from api import db
+
+class Conta(db.Model):
+    __tablename__ = "conta"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    nome = db.Column(db.String(50), nullable=False)
+    descricao = db.Column(db.String(100), nullable=False)
+    saldo = db.Column(db.Float, nullable=False)
